b07b3916b1d76e844fb51e6519727cd70abac544
Алексей Макаров <raminov.83@mail.ru>
2026-02-01 19:41:27 +0300

    init proj

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ChosePage from "./components/chosePage.vue";
+</script>
+
+<template>
+  <ChosePage />
+</template>
+
+<style></style>
---
+++ src/components/chosePage.vue
@@ -0,0 +1,5 @@
+<script lang="ts"></script>
+
+<template>
+  <p>chose page</p>
+</template>
